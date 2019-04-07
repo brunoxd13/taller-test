9cf8026b190738c904c56786880e716128af1492
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-04-07 11:50:02 -0300

    fix: adjusted to validate valid characters for name and password

--- next/src/modules/auth/containers/SigninContainer/SigninContainer.js
+++ next/src/modules/auth/containers/SigninContainer/SigninContainer.js
@@ -4,7 +4,7 @@ import { Form, Field } from 'react-final-form'
 import { Mutation } from 'react-apollo'
 import Router from 'next/router'
 
-import { combine, condition, required, minLength, email, equalsField } from 'app/lib/form/validation'
+import { combine, condition, required, minLength, email, equalsField, allowedStandardCharacters } from 'app/lib/form/validation'
 
 import { loginMutation, registerMutation } from './mutations'
 
@@ -16,9 +16,9 @@ export const labels = {
 }
 
 const validations = {
-  name: combine([required]),
+  name: combine([required, allowedStandardCharacters]),
   email: combine([required, email]),
-  password: combine([required, minLength(6)]),
+  password: combine([required, allowedStandardCharacters, minLength(6)]),
   passwordConfirm: condition(
     (value, { register }) => register,
     [required, equalsField('password', labels.password)],
